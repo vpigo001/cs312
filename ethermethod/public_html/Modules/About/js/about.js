799bc8126be273d631ba5d7eb30ac1c1181c72fb
vpigo001 <vpigo001@odu.edu>
2017-11-29 23:34:03 -0500

    -Created About page

---
+++ ethermethod/public_html/Modules/About/js/about.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    displayTitles();
+};
+
+displayTitles = function () {
+    $("#title").css("opacity", 1);
+    $("#subtitle").css("opacity", 1);
+    $("hr").css("opacity", 1);
+    $("hr").css("width", "35%");
+};
